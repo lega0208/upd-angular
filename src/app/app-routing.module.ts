d824325de6ec5eb1d2c31c9f243b252aaafc96c5
shoemakersteve <marclegault@live.com>
2021-11-24 18:13:36 -0500

    before re-adding jQuery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './views/overview/overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: '', redirectTo: '/overview', pathMatch: 'full' },
+
+  // todo: add rest of views
+
+  { path: '**', redirectTo: '/overview' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
